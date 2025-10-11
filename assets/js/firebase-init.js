c82c0bcf87d8a05d5f08177c9bf685db023746f5
Dat150802 <dat15082k2@gmail.com>
2025-10-11 13:41:39 +0700

    Update firebase-init.js

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -3,13 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 export const firebaseConfig = {
-  // DÁN GIÁ TRỊ THẬT VÀO ĐÂY
-  apiKey: "…",
+  apiKey: "AIzaSyDqkxqifDOka9v4Yduw8VN3ZjnhyJhvJrI",
   authDomain: "klc-ben-luc-crm.firebaseapp.com",
   projectId: "klc-ben-luc-crm",
   storageBucket: "klc-ben-luc-crm.appspot.com",
-  messagingSenderId: "…",
-  appId: "…"
+  messagingSenderId: "499809227125",
+  appId: "1:499809227125:web:a1854adad8026d82c8273c",
+  measurementId: "G-6GTQPBHTMW"
 };
 
 export const app = initializeApp(firebaseConfig);
